bcafbd2518c565437e448d69d42c8397b05ac93d
rauschma <axel@rauschma.de>
2012-02-25 04:40:15 +0100

    Quoting the URL argument, to be safe.

--- openurl.js
+++ openurl.js
@@ -1,24 +1,30 @@
+var format = require("util").format;
+var exec = require('child_process').exec;
+
 var command;
 
 switch (process.platform) {
     case "darwin":
-        command = "open"
+        command = "open '%s'";
         break;
     case "win32":
-        command = "start"
+        command = "start '%s'";
         break;
     case "linux2":
-        command = "xdg-open"
+        command = "xdg-open '%s'";
         break;
     default:
         throw new Error("Unsupported platform: "+process.platform);
 }
 
-var exec = require('child_process').exec;
-
+/**
+ * Error handling is deliberately minimal, as this function is to be easy to use for shell scripting
+ *
+ * @param url The URL to open
+ * @param callback A 0-argument function called after everything finishes successfully
+ */
 exports.open = function (url, callback) {
-    exec(command+" "+url, function (error, stdout, stderr) {
-        // Only rudimentary error handling, we want to be easy to use for shell scripting
+    exec(format(command, url), function (error, stdout, stderr) {
         if (error) {
             throw error;
         } else {
