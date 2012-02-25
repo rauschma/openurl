8f5280ada7556f27403cc8541ae9e30562d3479a
rauschma <axel@rauschma.de>
2012-02-25 14:29:18 +0100

    Better error handling for spawn (use an illegal URL to try it out).

--- openurl.js
+++ openurl.js
@@ -1,36 +1,47 @@
-var format = require('util').format,
-    spawn = require('child_process').spawn;
+var spawn = require('child_process').spawn;
 
 var command;
 
 switch(process.platform) {
-  case 'darwin':
-    command = 'open';
-    break;
-  case 'win32':
-    command = 'explorer.exe';
-    break;
-  case 'linux':
-    command = 'xdg-open';
-    break;
-  default:
-    throw new Error('Unsupported platform: ' + process.platform);
+    case 'darwin':
+        command = 'open';
+        break;
+    case 'win32':
+        command = 'explorer.exe';
+        break;
+    case 'linux':
+        command = 'xdg-open';
+        break;
+    default:
+        throw new Error('Unsupported platform: ' + process.platform);
 }
 
 /**
  * Error handling is deliberately minimal, as this function is to be easy to use for shell scripting
  *
  * @param url The URL to open
- * @param callback A 0-argument function called after everything finishes successfully
+ * @param callback A function with a single error argument. Optional.
  */
 
 exports.open = function(url, callback) {
-  spawn(command, [url], function(error, stdout, stderr) {
-    if (error) {
-      throw error;
-    }
-    else {
-      callback();
-    }
-  });
+    var child = spawn(command, [url]);
+    var errorText = "";
+    child.stderr.setEncoding('utf8');
+    child.stderr.on('data', function (data) {
+        errorText += data;
+    });
+    child.stderr.on('end', function () {
+        if (errorText.length > 0) {
+            var error = new Error(errorText);
+            if (callback) {
+                callback(error);
+            } else {
+                throw error;
+            }
+        } else if (callback) {
+            callback(error);
+        }
+    });
 }
+
+
