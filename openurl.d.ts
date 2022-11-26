1250af9d2a99e1c90b0ca5d4ad335ef368935316
antoine-coulon <coulon_antoine@outlook.fr>
2022-11-26 12:30:43 +0100

    feat: add minimalist TypeScript type definitions

---
+++ openurl.d.ts
@@ -0,0 +1,11 @@
+export declare function open(
+  url: string,
+  callback?: (error: Error) => void
+): void;
+
+export declare function mailto(
+  recipients: string[],
+  fields: Record<string, string>,
+  recipientsSeparator: string,
+  callback?: (error: Error) => void
+): void;
